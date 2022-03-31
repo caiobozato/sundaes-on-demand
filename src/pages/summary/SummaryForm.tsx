27b6c8a86e0e2e27d0ab1d06ad97a30ae314384f
Caio Bozato Aroca <bozato@ibm.com>
2022-03-31 11:08:37 -0300

    test: create unit tests for component

--- src/pages/summary/SummaryForm.tsx
+++ src/pages/summary/SummaryForm.tsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Popover, OverlayTrigger } from "react-bootstrap";
 
 export default function SummaryForm() {
   const [checked, setChecked] = useState(false);
 
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>No ice cream will be delivered</Popover.Body>
+    </Popover>
+  );
+
   const checkboxLabel = (
     <span>
-      I agree tp <span style={{ color: "blue" }}> Terms and conditions</span>
+      I agree to
+      <OverlayTrigger placement="top" overlay={popover}>
+        <span style={{ color: "blue" }}> Terms and conditions</span>
+      </OverlayTrigger>
     </span>
   );
 
--- src/pages/summary/test/SummaryForm.test.tsx
+++ src/pages/summary/test/SummaryForm.test.tsx
@@ -1,5 +1,11 @@
 import SummaryForm from "../SummaryForm";
-import { render, screen, fireEvent } from "@testing-library/react";
+import {
+  render,
+  screen,
+  fireEvent,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe("button and checkbox test suite", () => {
   test("checkbox is unchecked by default", () => {
@@ -42,5 +48,39 @@ describe("button and checkbox test suite", () => {
 
     fireEvent.click(checkbox);
     expect(button).toBeDisabled();
+  });
+});
+
+describe("popover tests", () => {
+  test("popover starts out hidden", () => {
+    render(<SummaryForm />);
+    const popover = screen.queryByText(/no ice cream will be delivered/i);
+    expect(popover).not.toBeInTheDocument();
+  });
+
+  test("popover appears when checkbox is hovered", () => {
+    render(<SummaryForm />);
+
+    const checkbox = screen.getByText(/terms and conditions/i);
+    userEvent.hover(checkbox);
+
+    const popover = screen.getByText(/no ice cream will be delivered/i);
+    expect(popover).toBeInTheDocument();
+  });
+
+  test("popover disappears when checkbox is unhovered", async () => {
+    render(<SummaryForm />);
+
+    const checkbox = screen.getByText(/terms and conditions/i);
+    userEvent.hover(checkbox);
+
+    const popover = screen.getByText(/no ice cream will be delivered/i);
+    expect(popover).toBeInTheDocument();
+
+    userEvent.unhover(checkbox);
+
+    await waitForElementToBeRemoved(() =>
+      screen.queryByText(/no ice cream will be delivered/i)
+    );
   });
 });
